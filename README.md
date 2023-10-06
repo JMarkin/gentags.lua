8a24a6a78e9c24c91a5a65eefd586de8f577fbad
jmarkin <me@jmarkin.ru>
2023-10-06 03:13:39 +0300

    rewrite plugin

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# Auto generate tags
-
-
-### Quck start
-
-1. Install plugin
-```lua
-use({
-    "JMarkin/gentags.lua",
-    config = function()
-        require("gentags").setup()
-    end,
-})
-```
-2. Generate and start auto generate on save `:GenTagsEnable
+# Auto generate tagfiles by ctags
--- lua/gentags/ctags.lua
+++ lua/gentags/ctags.lua
@@ -1,51 +1,35 @@
 local Job = require("plenary.job")
 local M = {}
 
-local current_running = nil
-local tag_added = false
-
-M.generate = function(cfg, file)
-  local args = vim.deepcopy(cfg.ctags.args)
-
-  table.insert(args, "-f")
-
-  local tagfile = cfg.cache.path:joinpath(cfg.tag_file_name):expand()
-  if cfg.ctags.in_root then
-    tagfile = cfg.root_dir:joinpath(cfg.tag_file_name):expand()
+M.generate = function(cfg, lang, tag_file, filepath)
+  local args = {
+    "--languages=" .. lang,
+    "-f",
+    tag_file:expand(),
+  }
+  for _, v in ipairs(cfg.args) do
+    table.insert(args, v)
   end
 
-  table.insert(args, tagfile)
-
-  if cfg.ctags.update_all then
+  if filepath then
+    table.insert(args, "-a")
+    table.insert(args, filepath)
+  else
     table.insert(args, "-R")
     table.insert(args, cfg.root_dir:expand())
-  else
-    table.insert(args, "-a")
-    table.insert(args, file)
   end
 
   local j = Job:new({
-    command = cfg.ctags.bin,
+    command = cfg.bin,
     args = args,
     on_exit = vim.schedule_wrap(function(job, code)
       if code ~= 0 then
         vim.notify(job._stderr_results, vim.log.levels.ERROR)
-        vim.pretty_print(args)
-      else
-        if not tag_added then
-          vim.cmd("set tags+=" .. tagfile)
-          tag_added = true
-        end
       end
-      current_running = nil
     end),
   })
 
-  if current_running ~= nil then
-    vim.loop.kill(current_running.pid, 3)
-  end
   j:start()
-  current_running = j
 end
 
 return M
